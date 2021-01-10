ead3174f30a870d6339ab22c665255eca0115776
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-11 03:59:47 +0500

    modals - setup default options standard for all modals of the product.

--- client/src/app/shared/services/util.service.ts
+++ client/src/app/shared/services/util.service.ts
@@ -1,7 +1,7 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { take } from 'rxjs/operators';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
 
 import { ConfirmComponent } from '../components/confirm/confirm.component';
 import { AlertComponent } from '@shared/components/alert/alert.component';
@@ -15,24 +15,30 @@ export { IAlertConfig, IConfirmConfig }
   providedIn: 'root'
 })
 export class UtilService {
+  modalDefaultOptions: ModalOptions = {
+    backdrop: 'static',
+    class: 'modal--lite',
+  };
 
   constructor(
     private modalService: BsModalService,
   ) { }
 
-  private modal(component: any, config: any, options: any): EventEmitter<any> {
+  public modal(component: any, config: any, options: any): EventEmitter<any> {
+    options = Object.assign({}, this.modalDefaultOptions, options);
     const modalRef: BsModalRef = this.modalService.show(component, options);
     modalRef.content.config = config;
+
     return modalRef.content
       .result
       .pipe(take(1));
   }
 
   public alert(config: IAlertConfig): EventEmitter<any> {
-    return this.modal(AlertComponent, config, {class: 'modal--lite'});
+    return this.modal(AlertComponent, config, {});
   }
 
   public confirm(config: IConfirmConfig): EventEmitter<any> {
-    return this.modal(ConfirmComponent, config, {class: 'modal--lite'});
+    return this.modal(ConfirmComponent, config, {});
   }
 }
