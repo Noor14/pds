602fb8609a8bb9fd5f5283d13d924d7a4bd16737
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-11 05:45:58 +0500

    doctors - update column names and data.

--- client/src/app/doctor/doctors.component.ts
+++ client/src/app/doctor/doctors.component.ts
@@ -8,20 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class DoctorsComponent implements OnInit {
 
   rows = [
-    {id: '00001', name: 'Dr Shahzad Nawaz', specialities: 'Gynelogist', contact: '03453145456' , experience: '7Years', totalContract: 8, memberSince: '10/May/2019'},
-    {id: '00002', name: 'Dr Sakinder Ali', specialities: 'Heart Specialist', contact: '03456825456', experience: '7Years', totalContract: 8, memberSince: '10/Feb/2019'},
-    {id: '00003', name: 'Dr Arif Shabbir', specialities: 'Neuro surgeon', contact: '03453111821' , experience: '3Years', totalContract: 1, memberSince: '01/Jan/2020'},
-    {id: '00004', name: 'Dr Zulqarain', specialities: 'Child Specialist', contact: '03476422556' , experience: '7Years', totalContract: 29, memberSince: '20/Aug/2018'},
-    {id: '00005', name: 'Dr Kumail Abbas', specialities: 'General Physician', contact: '03001166545' , experience: '7Years', totalContract: 12, memberSince: '10/Feb/2019'}
+    {id: '00005', name: 'Dr Kumail Abbas', specialities: 'General Physician', contact: '03001166545' , experienceStartYear: 2020, totalContract: 12, totalSale: 230000, memberSince: '10/Feb/2019'},
+    {id: '00001', name: 'Dr Shahzad Nawaz', specialities: 'Diabeties', contact: '03453145456' , experienceStartYear: 2008, totalContract: 30, totalSale: 100000, memberSince: '10/May/2019'},
+    {id: '00002', name: 'Dr Sikandar Ali', specialities: 'Heart Specialist', contact: '03456825456', experienceStartYear: 2015, totalContract: 8, totalSale: 70000, memberSince: '10/Feb/2019'},
+    {id: '00003', name: 'Dr Arif Shabbir', specialities: 'Neuro surgeon', contact: '03453111821' , experienceStartYear: 1994, totalContract: 5, totalSale: 150000,  memberSince: '01/Jan/2020'},
+    {id: '00004', name: 'Dr Zulqarain', specialities: 'Child Specialist', contact: '03476422556' , experienceStartYear: 2016, totalContract: 25, totalSale: 400000, memberSince: '20/Aug/2018'},
   ];
 
   columns = [
     { name: 'Doctor ID', prop: 'id'},
     { name: 'Name', prop: 'name'},
-    { name: 'specialities', prop: 'specialities'},
-    { name: 'contact', prop: 'contact'},
-    { name: 'Experience', prop: 'experience'},
-    { name: 'Total Contract', prop: 'totalContract'},
+    { name: 'Specialities', prop: 'specialities'},
+    { name: 'Total Contracts', prop: 'totalContract'},
+    { name: 'Total Sale', prop: 'totalSale'},
+    { name: 'Experience Since', prop: 'experienceStartYear'},
+    { name: 'Contact', prop: 'contact'},
     { name: 'Member Since', prop: 'memberSince'},
   ];
 
