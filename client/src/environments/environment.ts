62529680f25d2912da87d0538be74a92b97297a6
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-17 02:25:29 +0500

    project fe - implement environments.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,9 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+/** This is for DEV environment. i.e. localhost serve, and dev builds */
+
+import { IEnvironment } from './environment.model';
+import { environmentCommon } from './environment.common';
+
+export const environment: IEnvironment = { ...environmentCommon, ...{
+    production: false,
+
+    apiBaseURL: 'http://localhost:3402/api',
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
