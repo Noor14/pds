64426982aa5e5c5325950b28b0aa866ee5a906a0
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-17 23:18:11 +0500

    server - refactor and enhance mongodbConnect to work with vertical, and environment.

--- server/src/companies/companies.controller.js
+++ server/src/companies/companies.controller.js
@@ -6,19 +6,50 @@
 // app modules
 const respond = require('../shared/services/respond');
 const { connectDB } = require('../shared/services/mongodbConnect');
+
+// locals
+const config = {
+	collectionName: `companies`
+};
+
 /* exports */
-exports.getAll = getAll;
+module.exports = {
+	getAll,
+	getOne,
+};
 
 
 /* function declarations */
 
 // gets all companies
 async function getAll(req, res) {
-	const db = await connectDB();
+	// console.log('companies: getAll');
+
+	const db = await connectDB(req);
+	// console.log('companies: getAll: connectDB done:');
+
 	const data = await db
-	  .collection('companies')
+	  .collection(config.collectionName)
 	  .find({})
 	  .toArray();
+
+	respond.withSuccess(res, {
+		companies: data,
+	});
+}
+
+// gets target company info by id
+async function getOne(req, res) {
+	// console.log('companies: getOne');
+
+	const db = await connectDB(req);
+	// console.log('companies: getOne: connectDB done:');
+
+	const data = await db
+		.collection(config.collectionName)
+		.find({id: 'foo'})
+		.toArray();
+
 	respond.withSuccess(res, {
 		companies: data,
 	});
--- server/src/shared/services/mongodbConnect.js
+++ server/src/shared/services/mongodbConnect.js
@@ -1,33 +1,65 @@
+'use strict';
+
+// deps
 const MongoClient = require('mongodb').MongoClient;
-const DBName = 'demo';
-const uri =`mongodb+srv://qaswads-dev:verYeasYpassworD@cluster0.q82vv.mongodb.net/${DBName}?retryWrites=true&w=majority`;
+
+// app modules
+const environment = require('../../../environments/environment');
+
+// locals
 let connectionPromise = null;
 
+// exports
 exports.connectDB = connectDB;
 
-async function connectDB() {
+/* functions declarations */
+
+// generates vertical specific
+const generateVerticalDBInfo = (request) => {
+	const verticalDBName = request.vertical.credentials.mongoDbAtlas.databaseName;
+
+	return {
+		verticalDBName,
+		uri: `${environment.database.baseURI}/${verticalDBName}?retryWrites=true&w=majority`,
+	};
+};
+
+// TODO implement vertical specific DB connection.
+async function connectDB(request) {
+
+	// case: connection to DB is already in progress.
   if (connectionPromise) return connectionPromise;
 
+  const verticalDBInfo = generateVerticalDBInfo(request);
+
+  // case: proceed to perform the connection to DB
   connectionPromise = new Promise((resolve, reject) => {
-    const client = new MongoClient(uri, {
+	  console.log('connectDB: connecting...', verticalDBInfo);
+
+    const client = new MongoClient(verticalDBInfo.uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
+    // TODO review if this ever runs to true? because line#22 already has the cache serving.
+    // case: connection settled. proceed to awaiting requests.
     if (client.isConnected()) {
+    	console.log('connectDB: already isConnected !');
       return resolve(client);
-    } else {
-      client
-        .connect()
-        .then(() => {
-          return resolve(client.db(DBName));
-        })
-        .catch(err => {
-          console.log(err);
-          reject(err);
-        });
     }
+
+    // case: proceed to request to connect. respond requests with resolve or reject
+    client
+	    .connect()
+	    .then(() => {
+		    console.log('connectDB: connection: successful.');
+	      return resolve(client.db(verticalDBInfo.verticalDBName));
+	    })
+	    .catch(err => {
+		    console.log('connectDB: connection: failure.', err);
+	      reject(err);
+	    });
   });
 
   return connectionPromise;
-};
+}
