da1feb7df378866d2de724e12c394af7aa14a338
Noor Ali <nooraliyahoo95@gmail.com>
2021-01-17 20:39:44 +0500

    mongo connection

--- server/src/companies/companies.controller.js
+++ server/src/companies/companies.controller.js
@@ -5,7 +5,7 @@
 
 // app modules
 const respond = require('../shared/services/respond');
-
+const { connectDB } = require('../shared/services/mongodbConnect');
 /* exports */
 exports.getAll = getAll;
 
@@ -13,8 +13,13 @@ exports.getAll = getAll;
 /* function declarations */
 
 // gets all companies
-function getAll(req, res) {
+async function getAll(req, res) {
+	const db = await connectDB();
+	const data = await db
+	  .collection('companies')
+	  .find({})
+	  .toArray();
 	respond.withSuccess(res, {
-		companies: [],
+		companies: data,
 	});
 }
---
+++ server/src/shared/services/mongodbConnect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+const DBName = 'demo';
+const uri =`mongodb+srv://qaswads-dev:verYeasYpassworD@cluster0.q82vv.mongodb.net/${DBName}?retryWrites=true&w=majority`;
+let connectionPromise = null;
+
+exports.connectDB = connectDB;
+
+async function connectDB() {
+  if (connectionPromise) return connectionPromise;
+
+  connectionPromise = new Promise((resolve, reject) => {
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    if (client.isConnected()) {
+        console.log('connected')
+      return resolve(client);
+    } else {
+      client
+        .connect()
+        .then(() => {
+          return resolve(client.db(DBName));
+        })
+        .catch(err => {
+          console.log(err);
+          reject(err);
+        });
+    }
+  });
+
+  return connectionPromise;
+};
