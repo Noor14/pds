32b24bc495916a7f3297e15e21bfa99fba0a3525
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-17 18:05:49 +0500

    product - rename / refactor methods for curd service methods standard.

--- client/src/app/product/services/product.service.ts
+++ client/src/app/product/services/product.service.ts
@@ -1,21 +1,17 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { UtilService } from '@shared/services/util.service';
+import { HttpService } from '@shared/services/http.service';
 
-import { AddUpdateSearchProductComponent } from '../components/add-update-search-product/add-update-search-product.component';
+import { productsSettings } from '../products.constant';
 import {
-  ECRUDModalModes,
-  IAddUpdateSearchProductConfig,
   IProduct,
   IProductRaw,
   IGetAllProductsSuccessData, IAddProductSuccessData
 } from '../product.model';
-import { productsRawMock } from '../products.mock';
-import { productsSettings } from '../products.constant';
-import { HttpService } from '@shared/services/http.service';
 
 @Injectable({
   providedIn: 'root'
@@ -27,32 +23,32 @@ export class ProductService {
     private httpService: HttpService,
   ) { }
 
-  apiAddProduct(productRaw: IProductRaw): Observable<any> {
-    console.log('apiAddProduct:', productRaw);
+  apiAddOne(productRaw: IProductRaw): Observable<any> {
+    // console.log('apiAddOne:', productRaw);
 
     return this.httpService.post(`products`, productRaw)
       .pipe(
         map((data: IAddProductSuccessData) => {
-          data.product = this.parseOneProduct(data.product);
+          data.product = this.parseOne(data.product);
           return data;
         })
       );
   }
 
-  apiUpdateProduct(productRaw: IProductRaw): Observable<any> {
-    console.log('apiUpdateProduct:', productRaw);
+  apiUpdateOne(productRaw: IProductRaw): Observable<any> {
+    // console.log('apiUpdateProduct:', productRaw);
 
     return this.httpService.post(`products/${productRaw.id}`, productRaw)
       .pipe(
         map((data: IAddProductSuccessData) => {
-          data.product = this.parseOneProduct(data.product);
+          data.product = this.parseOne(data.product);
           return data;
         })
       );
   }
 
-  apiDeleteProduct(productRaw: IProductRaw): Observable<any> {
-    console.log('apiDeleteProduct:', productRaw);
+  apiDeleteOne(productRaw: IProductRaw): Observable<any> {
+    // console.log('apiDeleteOne:', productRaw);
 
     return this.httpService.delete(`products/${productRaw.id}`)
       .pipe(
@@ -63,41 +59,43 @@ export class ProductService {
       );
   }
 
-  apiGetOneProduct(productId: string): Observable<any> {
-    console.log('apiGetOneProduct:', productId);
+  apiGetOne(productId: string): Observable<any> {
+    // console.log('apiGetOne:', productId);
 
     return this.httpService.delete(`products/${productId}`)
       .pipe(
         map((data: any) => {
-          data.product = this.parseOneProduct(data.product);
+          data.product = this.parseOne(data.product);
           return data;
         })
       );
   }
 
-  apiFetchProductsList(): Observable<any> {
-    // console.log('apiFetchProductsList:');
+  apiGetList(): Observable<any> {
+    // console.log('apiGetList:');
     return this.httpService.get(`products`)
       .pipe(
 
-        // TODO remove when implemented from BE.
-        map((data: IGetAllProductsSuccessData) => {
-          data.products = data.products.length ? data.products : productsRawMock;
-          return data;
-        }),
+        // Use Mock data until BE implements real data.
+        // map((data: IGetAllProductsSuccessData) => {
+        //   data.products = data.products.length ? data.products : productsRawMock;
+        //   return data;
+        // }),
 
         map((data: IGetAllProductsSuccessData) => {
-          data.products = this.parseProducts(data.products);
+          data.products = this.parseList(data.products);
           return data;
         })
       );
   }
 
-  parseProducts(productsRaw: IProductRaw[]): IProduct[] {
-    return productsRaw.map((productRaw: IProductRaw) => this.parseOneProduct(productRaw));
+  parseList(productsRaw: IProductRaw[]): IProduct[] {
+    // console.log('parseList:', productsRaw);
+    return productsRaw.map((productRaw: IProductRaw) => this.parseOne(productRaw));
   }
 
-  parseOneProduct(productRaw: IProductRaw): IProduct {
+  parseOne(productRaw: IProductRaw): IProduct {
+    // console.log('parseOne:', productRaw);
     const product = Object.assign({
       tp: 0,
       discountPercent: 0,
