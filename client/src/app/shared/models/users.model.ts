96de9632ab895e6cb5e8e12b1ff64bb8b9bb4010
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-19 20:42:25 +0500

    users model - revised interface/schema for users and its types.

--- client/src/app/shared/models/users.model.ts
+++ client/src/app/shared/models/users.model.ts
@@ -1,14 +1,7 @@
 
-// export enum EUserType {
-//   Store, // customer Store user
-//   Doctor, // customer Doctor user
-//   SalesRep, // person from Distribution team.
-//   Admin // Admin/person from Distribution team.
-//   System // Admin/person from Qaswa Software team.
-// }
-
-export interface IUserRaw {
-  id: string;
+// abstraction only - no entity has this model. only to extend to User, Store, Store,
+export interface IUserCommonRaw {
+  id: number;
 
   // Here are "type" field possible values.
   // (Distribution team users starts from: 1 sales rep, 10 admin)
@@ -17,38 +10,48 @@ export interface IUserRaw {
   type: number,
 
   username: string;
-  firstName: string;
-  lastName: string;
+  areaId: number; // to be transform to Area/City Name
+  address: string; // complete simple 1 address string..
 
-  areaId: string; // to be transform to Area/City Name
-  address: string; // complete simple 1 address.
-  contacts: number[] // [123456789, 123456789]
-
-  /* doctor extension ==== */
+  /* user model extension ==== */
   // OR
-  /* store extension ==== */
+  /* store model extension ==== */
+  // OR
+  /* doctor model extension ==== */
 
-  createdOn: string; // memberSince.
-  createdBy: string; // id of the admin who created it
-  lastUpdatedOn: string;
-  lastUpdatedBy: string; // id of the admin who updated it
+  createdOn: string; // to be used as "Member Since". // (new Date()).toISOString(); // "2021-01-15T21:39:51.835Z"
+  createdBy: number; // id of the admin who created it
+  lastUpdatedOn: string; // "2021-01-15T21:39:51.835Z"
+  lastUpdatedBy: number; // id of the admin who updated it
 
   // dynamically calculated/evaluated from BE, not from DB.
   lastLoginOn: string;
 }
 
-// register custom properties here.
-export interface IUser extends IUserRaw {
-  customFullName: string;
+export interface IUserCommonParsed {
   customAreaName: string;
+}
+
+// for Admin, or Sales Rep of distribution team.
+export interface ISystemUserRaw extends IUserCommonRaw {
+  firstName: string;
+  lastName: string;
+
+  contacts: number[] // [123456789, 123456789]
+}
+
+export interface ISystemUserParsed extends IUserCommonParsed, ISystemUserRaw {
+  customFullName: string;
+  // other info like assignedTo, assignedBy, etc.
 }
 
 /* Doctor Model */
 
-export interface IDoctorRaw extends IUser {
+export interface IDoctorRaw extends ISystemUserParsed {
   doctorInfo: {
-    specialties: number[], // [0, 14, 25]
-    experienceSinceYear: number, // e.g 1990
+    certifications: number[], // [0, 14, 15] // for names check IDoctor
+    specialties: number[], // [0, 14, 25] // for names check IDoctor
+    experienceSince: string, // "2021-01-15T21:39:51.835Z"
 
     // dynamically calculated/evaluated from BE, not from DB.
     totalContracts: number,
@@ -57,31 +60,27 @@ export interface IDoctorRaw extends IUser {
 }
 
 // register custom added fields here
-export interface IDoctor extends IDoctorRaw {
-  customSpecialtiesNames: string[]; // ['Gyno', 'General Physcian', 'diabetes']
-  customExperienceYears: string; // 10 years, calculate from current year.
+export interface IDoctorParsed extends IUserCommonParsed, IDoctorRaw {
+  customCertificationsNames: string; // MBBS, FCPS, MCPS, BDS(Dentist), Other etc.
+  customSpecialtiesNames: string; // 'Gyno', 'General Physcian', 'diabetes'
+  customExperienceYears: string; // 10 years, calculate from doctorInfo.experienceSinceYear
   customContacts: string; // join number1 <br> number2
 }
 
 
 /* Store Model */
 
-// export enum IPersonType {
-//   Salesman,
-//   Manager,
-//   Owner
-// }
-
-export interface IPerson {
+export interface IPersonRaw {
   type: number; // sales person, Manager, Owner
-  name: string;
+  firstName: string;
+  lastName: string;
   phone: number;
 }
 
-export interface IStoreRaw extends IUser {
+export interface IStoreRaw extends IUserCommonRaw {
   storeInfo: {
     name: string; // e.g. "Al-Madina Pharmacy"
-    persons: IPerson[];
+    persons: IPersonRaw[];
 
     // dynamically calculated/evaluated from BE, not from DB.
     totalSaleAmount: number,
@@ -89,6 +88,6 @@ export interface IStoreRaw extends IUser {
 }
 
 // register custom added fields here
-export interface IStore extends IStoreRaw {
-  customPersons: string;
+export interface IStoreParsed extends IUserCommonParsed, IStoreRaw {
+  customPersons: string; // name <br> phone, name <br> phone. we may add roles as well.
 }
