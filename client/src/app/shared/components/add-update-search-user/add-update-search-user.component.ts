ce5432702b55e55c94ad11dbe6603d870a82efc9
israr <israrcs5@gmail.com>
2021-01-20 17:48:53 +0500

    ADD/UPDATE/SEARCH/USER - Implement hard code add-update-search-user form

--- client/src/app/shared/components/add-update-search-user/add-update-search-user.component.ts
+++ client/src/app/shared/components/add-update-search-user/add-update-search-user.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUpdateSearchUserComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  data = {
+    type: 1,
+    username: 'qaswa_admin',
+    firstName: 'Admin',
+    lastName: 'Qaswa',
+    email: 'admin@qaswa.com',
+    address: 'every where in every city',
+    area: 'Lahore',
+    contact: '03010123456',
+    storeName: 'Qaswa Medical Store',
+    specialties: 'Gynologist',
+    experienceSinceYear: '1990',
+  };
+
+  areas = [
+    {
+      name: 'Karachi'
+    }, {
+      name: 'Lahore'
+    }
+  ];
+
+  types = [
+    {
+      value: 1,
+      name: 'Store'
+    }, {
+      value: 2,
+      name: 'Doctor'
+    }, {
+      value: 3,
+      name: 'Client'
+    }
+  ];
+
+  submitForm(form: any) {
+    console.log(form)
+  }
 }
