edc93b3797c31f1ed12727edc8fd8e3fe399a4c1
Shahzad Nawaz <shahzadscs@gmail.com>
2021-01-22 18:09:42 +0500

    server - companies - refactor controller.

--- server/src/companies/companies.controller.js
+++ server/src/companies/companies.controller.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 // deps
@@ -19,43 +18,48 @@ module.exports = {
 
 // create company
 async function create(req, res) {
-		const data = new company(req.body)
-		data.save((error, response)=>{
-			  if(error){
-				respond.withFailure(res, 'record not inserted', 'fail');
-			  }else{
-				respond.withSuccess(res, {
-					companies: response
-				});
-			  }
-		  })
+	const data = new company(req.body)
+	data.save((error, response) => {
 
-}
-
-
-// gets all companies
-async function getAll(req, res) {
-	company.find({}, (error, response)=>{
-		if(error){
-		  respond.withFailure(res, 'record not inserted', 'fail');
-		}else{
-		  respond.withSuccess(res, {
-			  companies: response
-		  });
+		// case: DB error
+		if (error) {
+			respond.withFailure(res, 'record not inserted', error);
+			return;
 		}
+
+		respond.withSuccess(res, {
+			companies: response
+		});
 	})
 }
 
+// gets all companies
+async function getAll(req, res) {
+	company.find({}, (error, response) => {
+
+		// case: DB error
+		if (error) {
+			respond.withFailure(res, 'record not inserted', error);
+			return;
+		}
+
+		respond.withSuccess(res, {
+			companies: response
+		});
+	})
+}
 
 // gets target company info by id
 async function getOne(req, res) {
-	company.findOne({ id: { $eq: req.params.id } }, (error, response)=>{
-			if(error){
-				respond.withFailure(res, 'record not searchable', 'fail');
-			  }else{
-				respond.withSuccess(res, {
-					companies: response
-				});
-			  }
+	company.findOne({id: {$eq: req.params.id}}, (error, response) => {
+
+		// case: DB error
+		if (error) {
+			respond.withFailure(res, 'record not searchable', 'fail');
+		}
+
+		respond.withSuccess(res, {
+			companies: response
 		});
+	});
 }
