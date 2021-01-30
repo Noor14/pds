62cc188fe21afbddcd6a786b7db19195b60a06f2
arifshabbir <arifshabbir777@gmail.com>
2021-01-31 02:22:23 +0500

    public - public-abstract routing path issue fix

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('@core/public-abstracts.module').then(m => m.PublicAbstractsModule) },
+  { path: '', loadChildren: () => import('@root/app/public-abstract/public-abstracts.module').then(m => m.PublicAbstractsModule) },
   { path: 'user', loadChildren: () => import('@root/app/user-abstract/user-abstract.module').then(m => m.UserAbstractModule) },
 ];
 
