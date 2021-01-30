59faaee4fd144df733bd9d7fde1f9585bd085351
arifshabbir <arifshabbir777@gmail.com>
2021-01-31 02:09:14 +0500

    user - rename user abstract route

--- client/src/app/user-abstract/user-abstract.module.ts
+++ client/src/app/user-abstract/user-abstract.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '@shared/shared.module';
-import { UserRoutingModule } from './user-routing.module';
+import { UserAbstractRoutingModule } from './user-abstract-routing.module';
 
 import { UserAbstractComponent } from './user-abstract.component';
 import { UserHeaderComponent } from './components/user-header/user-header.component';
@@ -18,7 +18,7 @@ import { UserFooterComponent } from './components/user-footer/user-footer.compon
     CommonModule,
     SharedModule,
 
-    UserRoutingModule
+    UserAbstractRoutingModule
   ]
 })
 export class UserAbstractModule { }
