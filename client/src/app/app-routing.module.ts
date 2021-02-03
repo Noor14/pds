eefd2feace9a683f438ed5446944939ae6f8765a
Shahzad Nawaz <shahzadscs@gmail.com>
2021-02-04 04:43:30 +0500

    routes - rename /user/* routes to /app/*

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('@root/app/public-abstract/public-abstracts.module').then(m => m.PublicAbstractsModule) },
-  { path: 'user', loadChildren: () => import('@root/app/user-abstract/user-abstract.module').then(m => m.UserAbstractModule) },
+  { path: 'app', loadChildren: () => import('@root/app/user-abstract/user-abstract.module').then(m => m.UserAbstractModule) },
 ];
 
 @NgModule({
