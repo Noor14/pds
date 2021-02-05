8932f9969f8a60c94b1a289351080d8f6ae34093
arifshabbir <arifshabbir777@gmail.com>
2021-02-06 01:19:11 +0500

    constant - create contract mock

---
+++ client/src/app/contract/contracts.constant.ts
@@ -0,0 +1,6 @@
+export const contractList = [
+  {id: '0000101', name: 'Dr. Abu Bakar'},
+  {id: '0000102', name: 'Dr. Rutba'},
+  {id: '0000103', name: 'Dr. Mazahir'},
+  {id: '0000104', name: 'Dr. Yousaf'},
+];
