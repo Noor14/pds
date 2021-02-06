ae061015efad1e6d5e55dfb6df547f151be627e5
israr <israrcs5@gmail.com>
2021-02-07 01:25:38 +0500

    Headers - set headers for every request in interceptor.

--- client/src/app/core/interceptors/request.interceptor.ts
+++ client/src/app/core/interceptors/request.interceptor.ts
@@ -15,10 +15,11 @@ export class RequestInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // console.log('RequestInterceptor: intercept', request);
-
+    const userToken = 'secure-user-token';
     const modifiedRequest = request.clone({
       // responseType: 'json',
       withCredentials: true,
+      headers: request.headers.set('Authorization', `Bearer ${userToken}`),
     });
 
     return next.handle(modifiedRequest);
